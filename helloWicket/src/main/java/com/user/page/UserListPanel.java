ef2136241d16b4e29b0dbd74da56a9222bae924c
Administrator <Administrator@PC-20180121PYUK>
2018-03-30 18:32:36 +0800

    表格中使用复选框

--- helloWicket/src/main/java/com/user/page/UserListPanel.java
+++ helloWicket/src/main/java/com/user/page/UserListPanel.java
@@ -1,12 +1,14 @@
 package com.user.page;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormChoiceComponentUpdatingBehavior;
 import org.apache.wicket.ajax.markup.html.AjaxFallbackLink;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.behavior.AttributeAppender;
@@ -20,7 +22,13 @@ import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvid
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Check;
+import org.apache.wicket.markup.html.form.CheckGroup;
+import org.apache.wicket.markup.html.form.CheckGroupSelector;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.RepeatingView;
@@ -61,7 +69,7 @@ public class UserListPanel extends Panel{
 				// 创建删除标签
 				final Label delete = new Label(repeatingView.newChildId(),new Model<String>("删除"));
 				delete.setOutputMarkupId(true);
-				
+
 				Link<Void> link = new Link<Void>(repeatingView.newChildId()) {
 					private static final long serialVersionUID = 4362873349269038075L;
 
@@ -72,7 +80,7 @@ public class UserListPanel extends Panel{
 					}
 				};
 				link.setOutputMarkupId(true);
-				
+
 				// 添加操作组件
 				ActionPanel actionPanel = new ActionPanel(repeatingView.newChildId(), u);
 				repeatingView.add(actionPanel);
@@ -84,12 +92,12 @@ public class UserListPanel extends Panel{
 		};
 		// 添加分页
 		//		userView.setItemsPerPage(15);
-		add(userView);
+//		add(userView);
 		//		add(new PagingNavigator("pagingNavigator", userView));
 
 
 		// 在线编辑
-	/*	List<IColumn<User, String>> columns = new ArrayList<IColumn<User, String>>();
+		/*	List<IColumn<User, String>> columns = new ArrayList<IColumn<User, String>>();
 		columns.add(new AbstractColumn<User, String>(Model.of("Actions")){
 			private static final long serialVersionUID = -8068926012276059660L;
 
@@ -105,47 +113,57 @@ public class UserListPanel extends Panel{
 				"lastName"));
 		columns.add(new PropertyColumn<User, String>(Model.of("Home Phone"), "homePhone"));
 		columns.add(new PropertyColumn<User, String>(Model.of("Cell Phone"), "cellPhone"));
-		*/
-
+		 */
+		
+		Form<Void> dataForm = new Form<Void>("check");
+		dataForm.add(createGroup("group"));
+//		dataForm.add(userView);
+		add(dataForm);
 	}
+	
+	
+	// 生成表格的方法
+	private CheckGroup<User> createGroup(String id){
 
+		final CheckGroup<User> group = new CheckGroup<User>(id,new ArrayList<User>());
 
-	/*	 private CheckGroup<User> createGroup(String id){
+		group.add(new CheckGroupSelector("groupSelector"));
 
-	        final CheckGroup<User> group = new CheckGroup<User>(id,new ArrayList<User>());
+		//根据自己情况获得一个Element的List,生成ListView填充数据
+		List<User> list=iUserService.getUsers(new User());
+		List<User> checkUsers = new ArrayList<User>();
+		final ListView<User> listView = new ListView<User>("rows", list) {
+			private static final long serialVersionUID = 1L;
 
-	        group.add(new CheckGroupSelector("groupSelector"));
+			protected void populateItem(final ListItem<User> item) {
 
-	        //根据自己情况获得一个Element的List,生成ListView填充数据
-	        List<User> list=iUserService.getUsers(new User());
-	        List<User> checkUsers = new ArrayList<User>();
-	        final ListView<User> listView = new ListView("rows", list) {
+				//通过循环为ListView添加子控件
+				final User user = (User) item.getModelObject();
 
-	            protected void populateItem(final ListItem item) {
+				//为每一行添加chenk控件
+				item.add(new Check<User>("selected",new Model<User>(user)));
 
-	                //通过循环为ListView添加子控件
-	                final User user = (User) item.getModelObject();
+				//在这里面你可以通过同样的方式，添加自己的列
+				item.setOutputMarkupId(true);
+			}
+		};
 
-	                //为每一行添加chenk控件
-	                item.add(new Check("selected",new Model(user)));
+		group.add(listView);
 
-	                 在这里面你可以通过同样的方式，添加自己的列
-	                item.setOutputMarkupId(true);
-	            }
+		//当check被勾选上时，获取被勾选的一个对象，并且把对象放到提前定义好的集合里
+		group.add(new AjaxFormChoiceComponentUpdatingBehavior() {
+			private static final long serialVersionUID = 4449353651760076667L;
 
-	        };
-
-	        group.add(listView);
-
-	        //当check被勾选上时，获取被勾选的一个对象，并且把对象放到提前定义好的集合里
-	        group.add(new AjaxFormChoiceComponentUpdatingBehavior() {
-	            @Override
-	            protected void onUpdate(AjaxRequestTarget target) {
-	            	User uu =(User)group.getModelObject();
-	            	System.out.println(uu.getName());
-	            }
-	        });
-	        return group;
-	    }*/
+			@Override
+			protected void onUpdate(AjaxRequestTarget target) {
+				 List<User> checkedUsers = (List<User>)group.getModelObject();
+				 System.out.println("选中的个数: " + checkedUsers.size());
+			}
+		});
+		return group;
+	}
+	
+	
+	
 
 }
